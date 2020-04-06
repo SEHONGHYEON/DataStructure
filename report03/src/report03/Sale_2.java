ca8aa04783c61d1b52c0fa04e991d3a499f965c7
SAMSUNG <SAMSUNG@DESKTOP-8TN3U6J>
2020-04-06 15:19:23 +0900

    3주차 과제

---
+++ report03/src/report03/Sale_2.java
@@ -0,0 +1,15 @@
+package report03;
+
+public class Sale_2 {
+
+	public static void main(String[] args) {
+        int sale[][] = new int[][] {{63,84,140,130},
+        							{157,209,251,312}};
+		
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<4; j++)
+				System.out.printf("%d/4ºÐ±â: sale[%d][%d]= %d%n", j+1,i,j,sale[i][j]);
+			                  
+		}
+	}
+}
